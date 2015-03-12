ec2c31b52411407362922c582638f9178067d4a8
Nate Hunzaker <nate.hunzaker@gmail.com>
2015-03-12 11:41:34 -0600

    [add] First commit

---
+++ src/components/Boilerplate.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+let Boilerplate = React.createClass({
+
+  render() {
+    return (
+      <div>
+        <h1>Great things await</h1>
+        <p>Just follow your dreams.</p>
+      </div>
+    )
+  }
+
+})
+
+export default Boilerplate
---
+++ src/index.jsx
@@ -0,0 +1,6 @@
+import 'style/app'
+
+import React from 'react'
+import Boilerplate from 'components/Boilerplate'
+
+React.render(<Boilerplate />, document.getElementById('app'))
